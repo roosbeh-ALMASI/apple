e6b6f9c715b8f05a5b6de7bd22cf742697b4332f
Roosbeh Almasi <roosbeh@outlook.com>
2023-01-04 15:23:42 +0000

    this is a feature branch test

--- myApp.c
+++ myApp.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-// int function(char *);
+ int function(char *);
 
 int main(){
 
-//   function("Hello world..!\r\n");
+   function("Hello world..!\r\n");
 
    int i=0;
 
    for(i=0; i<20; i++)
     {
 #ifdef USE_SYSCALL
-       printf("Round %d: this is AAAAA roosbeh Github application..!\r\n", i);
+       printf("Round %d: this is AAAAA roosbeh Github, feature_1 application..!\r\n", i);
 #else
-       printf("Round %d: this is BBBBB roosbeh Github application..!\r\n", i);
+       printf("Round %d: this is BBBBB roosbeh Github feature_1 application..!\r\n", i);
 #endif
     }
 
