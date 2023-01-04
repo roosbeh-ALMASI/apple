6bf1036fa2fd5eb5209c1cf3ed916f1f14e28fcb
Roosbeh Almasi <roosbeh@outlook.com>
2023-01-04 12:02:15 +0000

    this is the first commit

---
+++ function.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int function(char *input)
+{
+  return printf("%s\n", input);
+}
---
+++ myApp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// int function(char *);
+
+int main(){
+
+//   function("Hello world..!\r\n");
+
+   int i=0;
+
+   for(i=0; i<20; i++)
+    {
+#ifdef USE_SYSCALL
+       printf("Round %d: this is AAAAA application..!\r\n", i);
+#else
+       printf("Round %d: this is BBBBB application..!\r\n", i);
+#endif
+    }
+
+    return 0;
+}
